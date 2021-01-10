fa6da4d42d84388ee1188440322330ff4df1ec11
lulu <1286487864@qq.com>
2021-01-10 16:41:26 +0800

    too much request

--- queryapi.py
+++ queryapi.py
@@ -1,17 +1,18 @@
 import requests
 import time
 import json
+from hoshino import aiorequests
 
 apiroot = 'https://help.tencentbot.top'
 
 def getprofile(viewer_id: int, interval: int = 1) -> dict:
-    reqid = json.loads(requests.get(f'{apiroot}/enqueue?target_viewer_id={viewer_id}').content.decode('utf8'))['reqeust_id']
+    reqid = json.loads(aiorequests.get(f'{apiroot}/enqueue?target_viewer_id={viewer_id}').content.decode('utf8'))['reqeust_id']
 
     if reqid is None:
         return "id err"
 
     while True:
-        query = json.loads(requests.get(f'{apiroot}/query?request_id={reqid}').content.decode('utf8'))
+        query = json.loads(aiorequests.get(f'{apiroot}/query?request_id={reqid}').content.decode('utf8'))
         status = query['status']
         if status == 'done':
             return query['data']
@@ -21,4 +22,4 @@ def getprofile(viewer_id: int, interval: int = 1) -> dict:
             return "queue"
 
 def queryarena(defs: list, page: int) -> dict:
-    return json.loads(requests.get(f'{apiroot}/arena?def={",".join([str(x) for x in defs])}&page={page}').content.decode('utf8'))
+    return json.loads(aiorequests.get(f'{apiroot}/arena?def={",".join([str(x) for x in defs])}&page={page}').content.decode('utf8'))
