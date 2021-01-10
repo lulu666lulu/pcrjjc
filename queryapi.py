152f4978a3e75e3a853f91b8554f3cda12677d60
lulu <1286487864@qq.com>
2021-01-10 18:08:29 +0800

    fix bug

--- queryapi.py
+++ queryapi.py
@@ -1,18 +1,17 @@
 import requests
 import time
 import json
-from hoshino import aiorequests
 
 apiroot = 'https://help.tencentbot.top'
 
 def getprofile(viewer_id: int, interval: int = 1, full: bool = False) -> dict:
-    reqid = json.loads(await aiorequests.get(f'{apiroot}/enqueue?full={full}&target_viewer_id={viewer_id}').content.decode('utf8'))['reqeust_id']
+    reqid = json.loads(equests.get(f'{apiroot}/enqueue?full={full}&target_viewer_id={viewer_id}').content.decode('utf8'))['reqeust_id']
 
     if reqid is None:
         return "id err"
 
     while True:
-        query = json.loads(await aiorequests.get(f'{apiroot}/query?request_id={reqid}').content.decode('utf8'))
+        query = json.loads(requests.get(f'{apiroot}/query?request_id={reqid}').content.decode('utf8'))
         status = query['status']
         if status == 'done':
             return query['data']
@@ -22,4 +21,4 @@ def getprofile(viewer_id: int, interval: int = 1, full: bool = False) -> dict:
             return "queue"
 
 def queryarena(defs: list, page: int) -> dict:
-    return json.loads(await aiorequests.get(f'{apiroot}/arena?def={",".join([str(x) for x in defs])}&page={page}').content.decode('utf8'))
+    return json.loads(requests.get(f'{apiroot}/arena?def={",".join([str(x) for x in defs])}&page={page}').content.decode('utf8'))
