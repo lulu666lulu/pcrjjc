c1c44d88b872c78ee7f726a4e6c1a83b58ad5d1c
huang12zheng <huang12zheng@163.com>
2021-01-14 06:57:10 +0800

    1. set a project 2. use aiohttp

--- queryapi.py
+++ queryapi.py
@@ -1,10 +1,18 @@
-from hoshino.aiorequests import get
+import aiohttp
+import asyncio
+
 import time
 import json
 
 apiroot = 'https://help.tencentbot.top'
 
 check_time_dict={}
+
+async def get(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            content = await response.read()
+            return json.loads(content)
 
 def isInPeriod(viewer_id: int) -> bool:
     value = check_time_dict[viewer_id] # exist by Branch statements in "getprofile"
@@ -17,13 +25,15 @@ def isInPeriod(viewer_id: int) -> bool:
 async def getprofile(viewer_id: int, interval: int = 1, full: bool = False) -> dict:
     reqid:int
     if not viewer_id in check_time_dict or not isInPeriod(viewer_id):
-        reqid = json.loads((await (await get(f'{apiroot}/enqueue?full={full}&target_viewer_id={viewer_id}')).content).decode('utf8'))['reqeust_id']
+        response=await get(f'{apiroot}/enqueue?full={full}&target_viewer_id={viewer_id}')
+        reqid = response['reqeust_id']
+
         if reqid is None:
             return "id err"
-        check_time_dict[viewer_id] = f"{reqid} {time.time()+60}" #秒的格式 # id 过期时间
+        check_time_dict[viewer_id] = f"{reqid} {time.time()+180}" #秒的格式 # id 过期时间
     reqid = check_time_dict[viewer_id].split()[0]
     while True:
-        query = json.loads(requests.get(f'{apiroot}/query?request_id={reqid}').content.decode('utf8'))
+        query= await get(f'{apiroot}/query?request_id={reqid}')
         status = query['status']
         if status == 'done':
             del check_time_dict[viewer_id]
